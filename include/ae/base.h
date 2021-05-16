a1e631327235a960c9490e7460dd02c875537548
Lucius Silvester <insofar2021@outlook.com>
2021-05-16 14:53:32 +0200

    restrict

--- include/ae/base.h
+++ include/ae/base.h
@@ -9,37 +9,32 @@
 #define AE_BASE_H
 
 #ifdef _MSC_VER
-#define WINCHECK
-
-#define AERESTRICT __restrict
-
-#ifndef _CRT_SECURE_NO_WARNINGS
-#define _CRT_SECURE_NO_WARNINGS
-#endif
-
-#else
-#define AERESTRICT restrict
-
+    #define WINCHECK
+    #ifndef _CRT_SECURE_NO_WARNINGS
+        #define _CRT_SECURE_NO_WARNINGS
+    #endif
 #endif
 
 #ifdef __cplusplus
+    #define AE_C_BEGIN extern "C" {
+    #define AE_C_END }
+    #if __cplusplus > 201703L
+        #define AECONSTEXPR20 constexpr
+    #else
+        #define AECONSTEXPR20 inline
+    #endif
+    #define AERESTRICT
 
-#define AE_C_BEGIN extern "C" {
-#define AE_C_END }
-
-#if __cplusplus > 201703L
-#define AECONSTEXPR20 constexpr
 #else
-#define AECONSTEXPR20 inline
+    #define AE_C_BEGIN
+    #define AE_C_END
+    #ifdef WINCHECK
+        #define AERESTRICT __restrict
+    #else
+        #define AERESTRICT restrict
+    #endif
+
 #endif
-
-#else
-
-#define AE_C_BEGIN
-#define AE_C_END
-
-#endif
-
-#define AEAPI __cdecl
+    #define AEAPI __cdecl
 
 #endif
