1eb2315253c0e72e6b67ce3b2d731c6bc7777da4
WS BAKJBBC <insofar2021@outlook.com>
2021-08-03 17:22:54 +0200

    util change

--- include/ae/util.hpp
+++ include/ae/util.hpp
@@ -1,54 +1,77 @@
-// © Nikola Stepanoski
+// © subludicrous
 // SPDX-License-Identifier: BSL-1.0
 
-#ifndef AE_UTIL_HPP
-#define AE_UTIL_HPP
+// miscellaneous
 
-#include <ae/string.h>
+#ifndef UTIL_HPP
+#define UTIL_HPP
+
 #include <string_view>
 #include <cstdio>
 #include <cstdint>
+#include <cstddef>
+#include <cctype>
 
 namespace ae {
+    // doesn't null terminate
+    template <std::size_t bits>
+    constexpr void int_to_bin(
+        char (&bbuf)[bits + 1u],
+        std::uintmax_t const val
+    ) {
+        static_assert(bits > 1u && bits <= (sizeof (std::uintmax_t) * 8));
+        char * binstr = bbuf;
+        for (auto i = std::uintmax_t{ 0x1u } << (bits - 1u); i; i >>= 1u) {
+            *binstr = (val & i) ? '1' : '0';
+            binstr++;
+        }
+    }
+
     // replace with std's in C++23
-    template<typename ch>
+    template <typename ch>
     [[nodiscard]]
-    constexpr bool contains(std::basic_string_view<ch> const sv, std::basic_string_view<ch> const what) noexcept {
+    constexpr bool contains(
+        std::basic_string_view<ch> const sv,
+        std::basic_string_view<ch> const what
+    ) noexcept {
         return sv.find(what) != sv.npos;
     }
 
     // replace with std's in C++20
-    template<typename ch>
+    template <typename ch>
     [[nodiscard]]
-    constexpr bool starts_with(std::basic_string_view<ch> const sv, std::basic_string_view<ch> const prefix) {
+    constexpr bool starts_with(
+        std::basic_string_view<ch> const sv,
+        std::basic_string_view<ch> const prefix
+    ) {
         return sv.substr(0, prefix.size()) == prefix;
     }
 
     [[nodiscard]]
-    constexpr bool is_power_of_2(unsigned long long const i) noexcept {
-        return (i & (i - 1U)) == 0U;
+    constexpr bool is_power_of_2(std::uintmax_t const i) noexcept {
+        return i != 0u && (i & (i - 1u)) == 0u;
     }
 
     [[nodiscard]]
     constexpr std::uint8_t lobyte(std::uint16_t const word) noexcept {
-        return static_cast<std::uint8_t>(word & 0x00FFU);
+        return static_cast<std::uint8_t>(word & 0x00FFu);
     }
 
     [[nodiscard]]
     constexpr std::uint8_t hibyte(std::uint16_t const word) noexcept {
-        return static_cast<std::uint8_t>(word >> 8U);
+        return static_cast<std::uint8_t>(word >> 8u);
     }
 
     [[nodiscard]]
     constexpr std::uint16_t reverse_endian(std::uint16_t const word) noexcept {
-        return static_cast<std::uint16_t>((lobyte(word) << 8U) + hibyte(word));
+        return static_cast<std::uint16_t>((lobyte(word) << 8u) + hibyte(word));
     }
 
-    template<size_t sz>
+    template <std::size_t sz>
     inline void print_hexbuf(char const (&str)[sz]) {
         for (auto elem = 0U; elem < sz; elem++) {
             auto const c = static_cast<unsigned char>(str[elem]);
-            const char cp = std::isalnum(c) ? c : '.';
+            char const cp = std::isalnum(c) ? c : '.';
             std::printf("Elem. %02u: 0x%02x\n", elem, c);
         }
     }
@@ -60,21 +83,23 @@ namespace ae {
         }
     }
 
-    template<size_t sz>
+    template <std::size_t sz>
     inline void print_binbuf(char const (&str)[sz]) {
-        char bbuf[9]{};
+        char bbuf[9];
+        bbuf[8] = '\0';
         for (auto elem = 0U; elem < sz; elem++) {
             auto const c = static_cast<unsigned char>(str[elem]);
-            ae::uint_to_binstr(bbuf, c, 8);
+            int_to_bin<8>(bbuf, c);
             std::printf("Elem. %02u: 0b%s\n", elem, bbuf);
         }
     }
 
     inline void print_binbuf(std::string_view const str) {
-        char bbuf[9]{};
+        char bbuf[9];
+        bbuf[8] = '\0';
         for (auto elem = 0U; elem < str.size(); elem++) {
             auto const c = static_cast<unsigned char>(str[elem]);
-            ae::uint_to_binstr(bbuf, c, 8);
+            int_to_bin<8>(bbuf, c);
             std::printf("Elem. %02u: 0b%s\n", elem, bbuf);
         }
     }
