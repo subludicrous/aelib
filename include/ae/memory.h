fc3ce4b74f94f424b794861e9d5221390e71610c
WS BAKJBBC <insofar2021@outlook.com>
2021-09-05 22:47:27 +0200

    new util

--- include/ae/memory.h
+++ include/ae/memory.h
@@ -6,10 +6,7 @@
 #ifndef __cplusplus
     #include <stddef.h>
 #else
-namespace std {
-    // ad hoc
-    using size_t = decltype(sizeof 0);
-}
+    #include <cstdlib>
 #endif
 
 AE_BEGIN
@@ -21,5 +18,68 @@ void *aligned_malloc(AE_STD size_t total_size, AE_STD size_t alignment);
 void aligned_free(void *ptr);
 
 AE_C_END
+
+#ifdef __cplusplus
+
+template <typename T, std::size_t amount = 1>
+[[nodiscard]]
+inline T *pmalloc() noexcept {
+    return static_cast<T *>(std::malloc(amount * sizeof(T)));
+}
+
+template <typename T>
+inline void pfree(T *const ptr) noexcept {
+    std::free(ptr);
+}
+
+template <
+    typename T,
+    T *(&alloc)() noexcept = pmalloc<T>,
+    void (&dealloc)(T *) noexcept = pfree<T>
+>
+class automem {
+public:
+    using value_type = T;
+    using pointer_type = value_type *;
+    pointer_type value;
+    [[nodiscard]]
+    constexpr automem() noexcept : value(alloc()) {}
+    constexpr ~automem() {
+        dealloc(value);
+    }
+    [[nodiscard]]
+    explicit constexpr operator pointer_type() const noexcept {
+        return value;
+    }
+    void operator=(automem const &) = delete;
+    void operator=(automem &&) = delete;
+};
+
+template <typename T, void (&fnclean)(T) noexcept = pfree>
+class autoclean {
+public:
+    using value_type = T;
+    value_type value;
+    [[nodiscard]]
+    constexpr autoclean(T const created) noexcept {
+        value = created;
+    }
+    constexpr ~autoclean() {
+        fnclean(value);
+    }
+    [[nodiscard]]
+    explicit constexpr operator value_type() const noexcept {
+        return value;
+    }
+    void operator=(autoclean const &) = delete;
+    void operator=(autoclean &&) = delete;
+};
+
+[[nodiscard]]
+constexpr char const *optstr(char const *const str) noexcept {
+    return str != nullptr ? str : "";
+}
+
+#endif 
 
 AE_END
